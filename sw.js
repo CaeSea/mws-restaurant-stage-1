747f78b3a5ea244f2b39b32b6e0f13cbe0f2eeef
Caeron Seabourne <caeking93@gmail.com>
2018-07-01 00:15:11 +0100

    Edited caching to use an array variable

--- sw.js
+++ sw.js
@@ -1,40 +1,39 @@
 let staticCacheName = 'resRevs-static-v1';
 
 self.addEventListener('install', function(event) {
+  const pathsToCache = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/restaurant.html?id=1',
+  '/restaurant.html?id=2',
+  '/restaurant.html?id=3',
+  '/restaurant.html?id=4',
+  '/restaurant.html?id=5',
+  '/restaurant.html?id=6',
+  '/restaurant.html?id=7',
+  '/restaurant.html?id=8',
+  '/restaurant.html?id=9',
+  '/restaurant.html?id=10',
+  'css/styles.css',
+  'js/main.js',
+  'js/dbhelper.js',
+  'js/restaurant_info.js',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg'
+];
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll(
-        [
-          '/',
-          '/index.html',
-          '/restaurant.html',
-          '/restaurant.html?id=1',
-          '/restaurant.html?id=2',
-          '/restaurant.html?id=3',
-          '/restaurant.html?id=4',
-          '/restaurant.html?id=5',
-          '/restaurant.html?id=6',
-          '/restaurant.html?id=7',
-          '/restaurant.html?id=8',
-          '/restaurant.html?id=9',
-          '/restaurant.html?id=10',
-          'css/styles.css',
-          'js/main.js',
-          'js/dbhelper.js',
-          'js/restaurant_info.js',
-          'data/restaurants.json',
-          'img/1.jpg',
-          'img/2.jpg',
-          'img/3.jpg',
-          'img/4.jpg',
-          'img/5.jpg',
-          'img/6.jpg',
-          'img/7.jpg',
-          'img/8.jpg',
-          'img/9.jpg',
-          'img/10.jpg'
-        ]
-      );
+      return cache.addAll(pathsToCache);
     }).catch(function() {
       console.log('Error registering cache.');
     })
